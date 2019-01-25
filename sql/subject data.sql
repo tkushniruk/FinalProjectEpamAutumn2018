2ccd50d0dc6a19d39df02d8d4c4306797cfe53ef
SSHkiper <C:\Users\windd\AppData\Roaming\The Bat! Pwd>
2019-01-25 20:42:37 +0200

    updated files

--- sql/subject data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `Subject` (`name_ru`, `name_eng`) VALUES ("Математика", "Math");
-INSERT INTO `Subject` (`name_ru`, `name_eng`) VALUES ("Литература", "Literature");
-INSERT INTO `Subject` (`name_ru`, `name_eng`) VALUES ("Биология", "Biology");
-INSERT INTO `Subject` (`name_ru`, `name_eng`) VALUES ("Химия", "Chemistry");
-INSERT INTO `Subject` (`name_ru`, `name_eng`) VALUES ("Украинский язык", "Ukrainian");
-INSERT INTO `Subject` (`name_ru`, `name_eng`) VALUES ("История", "History");
-INSERT INTO `Subject` (`name_ru`, `name_eng`) VALUES ("Физика", "Physics");
-INSERT INTO `Subject` (`name_ru`, `name_eng`) VALUES ("Английский язык", "English");
